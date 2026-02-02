bf951afe836e0ce8389c18cf57a586c7732d938e
Nihal <annihal55@gmail.com>
2026-02-03 03:05:28 +0600

    Add TestDino reporter to Playwright framework

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['list']
+    ['list'],
+    ['@testdino/playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
