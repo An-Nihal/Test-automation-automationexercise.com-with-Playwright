93a242eab8390e21086c46f69ec4a43fcc210500
Nihal <annihal55@gmail.com>
2026-02-03 03:13:27 +0600

    Remove TestDino reporter from framework

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,8 +18,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['list'],
-    ['@testdino/playwright']
+    ['list']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
